4f283195a42359f4c609bc5a4c80c1f80e171fee
Bruno Fracaro <bruno.fracaro.alcantara>
2023-12-01 14:08:50 -0300

    create context

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -194,64 +194,72 @@ import HomeScreen from './screens/homeScreen';
 import NewBetScreen from './screens/newBetScreen';
 import ProfileScreen from './screens/profileScreen';
 
+import MetamaskContext from './context/metamask';
+
 const home = require('./assets/home.png');
 const numbers = require('./assets/numbers.png');
 const user = require('./assets/user.png');
 
 const Tabs = AnimatedTabBarNavigator();
 
-export default () => (
-  <NavigationContainer theme={{colors: {background: '#1A1917'}}}>
-    <Tabs.Navigator
-      tabBarOptions={{
-        activeTintColor: '#fff',
-        inactiveTintColor: '#fff',
-        activeBackgroundColor: '#2a2926',
-      }}
-      appearance={{
-        tabBarBackground: '#3e3d38',
-        dotCornerRadius: 10,
-        dotSize: 'small',
-      }}>
-      <Tabs.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              resizeMode="contain"
-              source={home}
-              style={{width: 20, height: 20}}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="New Bet"
-        component={NewBetScreen}
-        options={{
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              resizeMode="contain"
-              source={numbers}
-              style={{width: 20, height: 20}}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: ({focused, color, size}) => (
-            <Image
-              resizeMode="contain"
-              source={user}
-              style={{width: 20, height: 20}}
-            />
-          ),
-        }}
-      />
-    </Tabs.Navigator>
-  </NavigationContainer>
-);
+export default () => {
+  const metamaskHook = React.useState('');
+
+  return (
+    <NavigationContainer theme={{colors: {background: '#1A1917'}}}>
+      <MetamaskContext.Provider value={metamaskHook}>
+        <Tabs.Navigator
+          tabBarOptions={{
+            activeTintColor: '#fff',
+            inactiveTintColor: '#fff',
+            activeBackgroundColor: '#2a2926',
+          }}
+          appearance={{
+            tabBarBackground: '#3e3d38',
+            dotCornerRadius: 10,
+            dotSize: 'small',
+          }}>
+          <Tabs.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <Image
+                  resizeMode="contain"
+                  source={home}
+                  style={{width: 20, height: 20}}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="New Bet"
+            component={NewBetScreen}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <Image
+                  resizeMode="contain"
+                  source={numbers}
+                  style={{width: 20, height: 20}}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <Image
+                  resizeMode="contain"
+                  source={user}
+                  style={{width: 20, height: 20}}
+                />
+              ),
+            }}
+          />
+        </Tabs.Navigator>
+      </MetamaskContext.Provider>
+    </NavigationContainer>
+  );
+};
--- AwesomeProject/screens/profileScreen.js
+++ AwesomeProject/screens/profileScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text, View, ScrollView, Image, TouchableOpacity, Linking } from 'react-native';
+import { Text, View, ScrollView, Image, TouchableOpacity, Linking, ActivityIndicator } from 'react-native';
 
 import MetaMaskSDK from '@metamask/sdk';
 import BackgroundTimer from 'react-native-background-timer';
 
+import MetamaskContext from '../context/metamask';
+
 import '@ethersproject/shims';
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 
 import { myPallete } from '../components/colorPallete';
 
@@ -13,63 +15,76 @@ const meta = require('../assets/Metamask-icon.png')
 
 const ProfileScreen = () => {
 
-  const [user, setUser] = React.useState('')
-  const [balance, setBalance] = React.useState('')
+  const [metamaskCxt, setMetamaskCxt] = React.useContext(MetamaskContext);
+  const [render, setRender] = React.useState(false)
+  const [loading, setLoading] = React.useState(true)
 
-  const [eth, setEth] = React.useState();
+  const checkConection = async () => {
+    if (!metamaskCxt.eth) {
+      connect()
+    }
+  }
 
-  React.useEffect(() => {
-    const exec = async () => {
-      console.log('restart');
-      const MMSDK = new MetaMaskSDK({
-        openDeeplink: link => {
-          Linking.openURL(link); // Use React Native Linking method or another way of opening deeplinks.
-        },
-        timer: BackgroundTimer, // To keep the dapp alive once it goes to background.
-        dappMetadata: {
-          name: 'My dapp', // The name of your dapp.
-          url: 'https://mydapp.com', // The URL of your website.
-        },
-      });
+  const connect = async () => {
 
-      await MMSDK.init();
+    setLoading(true)
 
-      const ethereum = MMSDK.getProvider();
-      setEth(ethereum);
-    };
+    console.log('restart');
+    const MMSDK = new MetaMaskSDK({
+      openDeeplink: link => {
+        Linking.openURL(link); // Use React Native Linking method or another way of opening deeplinks.
+      },
+      timer: BackgroundTimer, // To keep the dapp alive once it goes to background.
+      dappMetadata: {
+        name: 'My dapp', // The name of your dapp.
+        url: 'https://mydapp.com', // The URL of your website.
+      },
+    });
 
-    exec();
-  }, []);
+    await MMSDK.init();
 
-  const connect = async() => {
-    const accounts = await eth.request({method: 'eth_requestAccounts'});
+    const ethereum = MMSDK.getProvider();
 
-    setUser(accounts[0])
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 
-    const provider = new ethers.BrowserProvider(eth);
+    const provider = new ethers.BrowserProvider(ethereum);
 
     const balance = await provider.getBalance(
-      'https://eth-goerli.g.alchemy.com/v2/fimlv4n0QGOOIbJKbIqTz6y-Pa4wIgIt',
+      ethereum.selectedAddress
     );
 
     const balanceInETH = ethers.formatEther(balance);
 
-    setBalance({balanceInETH});
+    const newContext = {
+      eth: ethereum,
+      address: accounts[0],
+      provider: provider,
+      balance: balanceInETH
+    }
+
+    setMetamaskCxt(newContext);
+    setRender(!render)
+    setLoading(false)
   }
 
   return (
     <ScrollView style={{ flex: 1, backgroundColor: myPallete.backgroundBlack, paddingTop: 50 }}>
       <Text style={{ color: myPallete.mainGreen, fontSize: 28, marginLeft: 18 }}>Profile</Text>
-      <TouchableOpacity onPress={() =>connect()} style={{ padding: 14, backgroundColor: '#373737', borderRadius: 20, width: '95%', marginTop: 14, alignSelf: 'center', alignItems: 'center', justifyContent: 'center', borderColor: user ? myPallete.mainGreen : myPallete.mainOrange, borderWidth: 0.7 }}>
+      <TouchableOpacity disabled={metamaskCxt.address} onPress={() => checkConection()} style={{ padding: 14, backgroundColor: '#373737', borderRadius: 20, width: '95%', marginTop: 14, alignSelf: 'center', alignItems: 'center', justifyContent: 'center', borderColor: metamaskCxt.address ? myPallete.mainGreen : myPallete.mainOrange, borderWidth: 0.7 }}>
         <Image resizeMode='contain' source={meta} style={{ width: 80, height: 80, borderRadius: 50 }} />
-        {user ? (
+        {metamaskCxt.address ? (
           <>
-            <Text style={{ fontSize: 14, color: '#ffffff', fontWeight: '500', marginTop: 10 }}>{user}</Text>
-            <Text style={{ fontSize: 14, color: '#ffffff', fontWeight: '500', marginTop: 10 }}>{balance} eth</Text>
+            <Text style={{ fontSize: 14, color: '#ffffff', fontWeight: '500', marginTop: 10 }}>{metamaskCxt.address}</Text>
+            <Text style={{ fontSize: 14, color: '#ffffff', fontWeight: '500', marginTop: 10 }}>{Math.round(metamaskCxt.balance * 10000) / 10000} eth</Text>
           </>
-        ): (
+        ) : (
           <Text style={{ fontSize: 20, color: '#ffffff', fontWeight: '500', marginTop: 10 }}>Connect to you MetaMask account</Text>
         )}
+        {loading ? (
+          <View style={{position: 'absolute', width: '100%', borderRadius: 20, backgroundColor: '#000000aa', height: '100%', justifyContent: 'center'}}>
+            <ActivityIndicator size="large" color={myPallete.mainGreen}/>
+          </View>
+        ): undefined}
       </TouchableOpacity>
       <View style={{ width: 10, height: 100 }} />
     </ScrollView>
